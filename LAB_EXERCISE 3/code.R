4b1e56482e16cc21f4c5d38bb20b34b56ed2ec27
Saranya M <saranyamuthuraj005@gmail.com>
2026-02-09 08:46:59 +0530

    Update code.R

--- LAB_EXERCISE 3/code.R
+++ LAB_EXERCISE 3/code.R
@@ -1,1 +1,22 @@
+# EXPERIMENT: Multivariate Data Visualization
+# STUDENT ROLL NO: 23BAD104
+
+head(x3_healthcare_data)
+colnames(x3_healthcare_data)
+
+x3_healthcare_data$AgeGroup <- cut(
+  x3_healthcare_data$Age,
+  breaks = c(19, 35, 50, 70),
+  labels = c("Young", "Middle", "Senior")
+)
+
+pairs(
+  x3_healthcare_data[, c("Age", "BMI", "Glucose_Level", "Blood_Pressure")],
+  col = x3_healthcare_data$AgeGroup,
+  main = "Scatter Plot Matrix of Health Indicators"
+)
+
+cor(
+  x3_healthcare_data[, c("Age", "BMI", "Glucose_Level", "Blood_Pressure")]
+)
 
